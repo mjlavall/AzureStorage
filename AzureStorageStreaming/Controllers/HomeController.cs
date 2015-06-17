ebd2c7d4b95744c461bce73d055917c4e724f6cd
Michael LaValley <mlavalley@saturnsys.com>
2015-06-17 17:43:46 -0500

    Added web portal

---
+++ AzureStorageStreaming/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AzureStorageStreaming.Models;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageStreaming.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var files = new Dictionary<string, string>();
+            foreach (var blob in CloudAccess.GetFiles())
+            {
+                if (blob is CloudBlockBlob)
+                {
+                    var blockBlob = (CloudBlockBlob)blob;
+                    files.Add(blockBlob.Name, blockBlob.Uri.ToString());
+                }
+            }
+            return View(files);
+        }
+        // GET: /Home/Player/GOPRO015.MP4
+        public ActionResult Player(string id)
+        {
+            ViewBag.FileName = id;
+            return View();
+        }
+    }
+}
---
+++ AzureStorageStreaming/Models/CloudAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace AzureStorageStreaming.Models
+{
+    public static class CloudAccess
+    {
+        private static CloudStorageAccount StorageAccount
+        {
+            get { return CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString")); }
+        }
+        private static CloudBlobClient BlobClient
+        {
+            get
+            {
+                var blobClient = StorageAccount.CreateCloudBlobClient();
+                blobClient.DefaultRequestOptions.SingleBlobUploadThresholdInBytes = 1024 * 1024;
+                return blobClient;
+            }
+        }
+        private static CloudBlobContainer Container
+        {
+            get
+            {
+                var container = BlobClient.GetContainerReference("gopro");
+                container.CreateIfNotExists();
+                container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                return container;
+            }
+        }
+        private const int BlockSize = 256 * 1024;
+
+        public static IEnumerable<IListBlobItem> GetFiles()
+        {
+            return Container.ListBlobs();
+        }
+    }
+}
